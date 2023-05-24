2b64b6eb5a68f92ab8101762c2b138029298814b
Akylhus <akylus5@gmail.com>
2023-05-24 23:58:58 +0200

    update 24/05

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //variable & constante declaration
-
-const openWindow = document.querySelector(".newWindow");
-
+let i= 0;
+const itemFilter = document.querySelector(".newWindow");
 
 //functions & events
 
@@ -9,35 +8,8 @@ const openWindow = document.querySelector(".newWindow");
 
 window.onload = function()
 {
-
-
-}    
-
-//click opening
-openWindow.addEventListener
-("click", function()
-{
-    newWindow();
-}
-);
-
-let i= 0;
-fetch("http://localhost:5678/api/works")
-    .then(reponse => reponse.json())
-    .then(body => 
-        {
-        for (let i = 0; i < body.length; i++)
-            {
-            console.log(body.length);
-            const figureElement = document.createElement("figure");
-            const imageElement = document.createElement("img");
-            imageElement.src = body[i].imageUrl;
-            const divGallery = document.querySelector(".gallery");
-            divGallery.appendChild(figureElement);
-            figureElement.appendChild(imageElement);
-            }
-        })
-
+document.querySelector(".gallery").innerHTML = "";
+//FILTER BUTTON----------------------------------------------------------        
 fetch("http://localhost:5678/api/categories")
         .then(reponse => reponse.json())
         .then(body => 
@@ -46,7 +18,54 @@ fetch("http://localhost:5678/api/categories")
                 const divFilter = document.createElement("div");
                 divFilter.innerText = body[i].name;
                 divFilter.classList.add("filter");
+                divFilter.classList.add("filter"+body[i].id);
                 const divFilters = document.querySelector(".filters");
                 divFilters.appendChild(divFilter);
             }    
         })
+
+//IMAGE CREATION--------------------------------------------------------
+
+function generateImage ()
+{
+fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(body => 
+        {
+        for (let i = 0; i < body.length; i++)
+            {
+            const figureElement = document.createElement("figure");
+            const imageElement = document.createElement("img");
+            imageElement.src = body[i].imageUrl;
+            imageElement.alt = body[i].title;
+            const figcaptionElement = document.createElement("figcaption");
+            figcaptionElement.innerText = body[i].title;
+            const divGallery = document.querySelector(".gallery");
+            divGallery.appendChild(figureElement);
+            figureElement.appendChild(imageElement);
+            figureElement.appendChild(figcaptionElement);
+            }
+        })
+
+}    
+generateImage();
+}
+
+// FILTER ITEMS-----------------------------------------------------------
+
+fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(body => 
+        {
+        itemFilter.addEventListener
+        (
+        "click", function()
+            {
+                const objetsFiltres = body[1].categoryId;
+                return body.categoryId;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+                generateImage(objetsFiltres);
+            }
+        );
+   
