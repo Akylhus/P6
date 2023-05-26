0f6384c708ad78336b621246a6adfc9fd91915fd
Akylhus <akylus5@gmail.com>
2023-05-26 17:48:53 +0200

    update 26/05

---
+++ assets/log.js
@@ -0,0 +1,9 @@
+//variable & constante declaration
+const responseUsers = await fetch("http://localhost:5678/api/users");
+const users = await responseUsers.json();
+
+//functions & events
+
+
+
+//code entry point
