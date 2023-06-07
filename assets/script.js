e12f2e7dbe1aba2378be565b2d3d6cac514d61cf
Akylhus <akylus5@gmail.com>
2023-06-07 19:54:45 +0200

    update 7/6

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const responseWorks = await fetch("http://localhost:5678/api/works");
 const works = await responseWorks.json();
 const responseCategory = await fetch("http://localhost:5678/api/categories");
 const categories = await responseCategory.json();
-const openModal = document.querySelector(".modif");
+const modif = document.querySelector(".modif");
 const modal = document.querySelector(".modal");
 const modalWrapper = document.querySelector(".modal-wrapper");
 
@@ -100,30 +100,37 @@ hotelFilter.addEventListener
 const allFilter = document.querySelector(".filter");
 allFilter.addEventListener
 (
-"click", function()
+    "click", function()
     {
         document.querySelector(".gallery").innerHTML = "";
         generateImage(works);
     }
-);
+    );
+    
 
-//OPEN MODAL
-openModal.addEventListener
-(
-    "click", function()
+
+    //OPEN MODAL
+    const modalGallery = document.querySelector(".gallery2");
+    const openModal = function(e)
     {
+        e.preventDefault();
         modal.style.display = "flex";
-        modalWrapper.style.display = "block";
-        modalWrapper.classList.add("gallery");
-        modalWrapper.classList.add("gallery2");
+        modal.removeAttribute("aria-hidden");
+        modal.setAttribute("aria-modal", "true");
+        modalGallery.classList.add("gallery");
         generateImage(works);
-        modal.addEventListener
-        (
-            "click", function()
-            {
-                modal.style.display = "none";
-                modalWrapper.style.display = "none";
-            }
-        )
+        modal.addEventListener("click", closeModal);
     }
-)
+
+    const closeModal = function (e)
+    {
+        if (modal === null) return
+        e.preventDefault();
+        document.querySelector(".gallery").innerHTML = "";
+        modal.style.display = "none";
+        modal.setAttribute("aria-hidden", "true");
+        modal.removeAttribute("aria-modal");
+        modal.removeEventListener("click", closeModal);
+    }
+
+    modif.addEventListener("click", openModal);
